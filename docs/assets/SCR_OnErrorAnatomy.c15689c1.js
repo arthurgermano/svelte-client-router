56b1c093a1e1036358fc470c4bc02e13c9e9daa5
Arthur <arthurgermano@gmail.com>
2022-09-13 12:51:53 -0300

    version 2 docs

--- docs/assets/SCR_OnErrorAnatomy.c15689c1.js
+++
@@ -1,5 +0,0 @@
-import{S as E,i as C,s as S,e as f,a as u,b as m,c as o,n as b,g as s}from"./index.fc5ef2be.js";function _(x){let r,c,n,a,l,p,i;return{c(){r=f("h4"),r.textContent="Anatomy of the On Error Function",c=u(),n=f("p"),n.textContent="Lets check the declaration of an onError function:",a=u(),l=f("pre"),l.innerHTML=`<b class="scr-b">// Example of On Error function declaration</b>
-(error) =&gt; { 
-  console.error(error);
-}
-`,p=u(),i=f("ul"),i.innerHTML="<li><b>error:</b> The error object containing the error information</li>",m(r,"class","scr-h4"),m(n,"class","scr-text-justify"),m(l,"class","scr-pre")},m(e,t){o(e,r,t),o(e,c,t),o(e,n,t),o(e,a,t),o(e,l,t),o(e,p,t),o(e,i,t)},p:b,i:b,o:b,d(e){e&&s(r),e&&s(c),e&&s(n),e&&s(a),e&&s(l),e&&s(p),e&&s(i)}}}class L extends E{constructor(r){super(),C(this,r,null,_,S,{})}}export{L as S};
--- docsproj/vite.config.js
+++ docsproj/vite.config.js
@@ -1,10 +1,30 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import packageJson from "../package.json";
+import css from "rollup-plugin-css-only";
 
 process.env.VITE_SCR_VERSION = packageJson.version;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [
+    svelte({
+      emitCss: true,
+    }),
+  ],
+  build: {
+    commonjsOptions: {
+      sourceMap: true,
+    },
+    rollupOptions: {
+      output: {
+        sourcemap: true,
+        format: "iife",
+        name: "app",
+        // file: "dist/build/bundle.js",
+      },
+      plugins: [css({ output: "bundle.css" })],
+      inlineDynamicImports: true,
+    },
+  },
+});
